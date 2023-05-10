dbd19a344f8cf12a21127897fe3044f1838df198
Cicchelli <cicchelli.stephan@gmail.com>
2023-05-10 17:05:38 -0300

    req 10

--- src/challenges.js
+++ src/challenges.js
@@ -107,7 +107,22 @@ function decode(texto) {
 }
 
 // Desafio 10 - Crie a função techList
+function techList(listaTech, name) {
+  if (listaTech.length === 0) {
+  return [];
+}
 
+let resultArray = [];
+listaTech.sort();
+for (let skill of listaTech) {
+  let objeto = {
+    tech: skill,
+    name: `${name}`,
+  };
+  resultArray.push(objeto);
+}
+return resultArray;
+}
 
 // Não modifique essas linhas
 module.exports = {
